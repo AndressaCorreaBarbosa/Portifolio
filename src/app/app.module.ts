bc1f894ebec43183687d6087589965011d73228a
AndressaCorreaBarbosa <andressacorrea2111@gmail.com>
2023-12-17 18:02:02 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MeComponent } from './components/me/me.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HeaderComponent,
+    MeComponent,
+    CursosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    RouterModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.css']
 })
 export class NavComponent {
 
